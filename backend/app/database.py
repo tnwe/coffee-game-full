0e44d63f43f912d215b98eec5359a209db8f6c4b
Nicolas Wedraogo <Nicolas@ul-propuls.fr>
2025-12-15 14:37:41 +0100

    message database debug

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv("postgresql://coffee_game_db_uanh_user:DUKZ0DlqPj4YlNoRhpDP4pC0rt7lUVHx@dpg-d4u3i0mmcj7s73drdolg-a/coffee_game_db_uanh")
 
-print(">>> DATABASE_URL =", DATABASE_URL)
+print(">>> DATABASE_URL =", SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
