61e9dc7ef210915c5c45a9dcf57818b059a64ecc
Nicolas Wedraogo <Nicolas@ul-propuls.fr>
2025-12-15 14:12:32 +0100

    message database debug

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,6 +8,8 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+print(">>> DATABASE_URL =", DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
