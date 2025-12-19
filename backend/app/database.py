a8ecf4e3b65f04f87d14b4fc6a1c70d2d0d95d01
Nicolas Wedraogo <Nicolas@ul-propuls.fr>
2025-12-19 11:25:06 +0100

    modif database

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,11 +6,18 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 print(">>> DATABASE_URL =", DATABASE_URL)
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-
+# SQLite
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    # PostgreSQL (Render)
+    engine = create_engine(
+        DATABASE_URL,
+        pool_pre_ping=True
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
